1e9afb4a649d7dcea39a239492b557f905f891f7
Edwin Saa <edwin.saa@udla.edu.ec>
2025-05-01 07:13:24 -0500

    Add project files.

---
+++ EdwinSaa_MVC_Veterinaria_ExamenP1_P4/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EdwinSaa_MVC_Veterinaria_ExamenP1_P4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
